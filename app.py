538e303697d996764493c31bc65772291fec46f2
easyundercover <belenfpineyro@gmail.com>
2022-08-12 19:46:43 -0300

    updated

--- app.py
+++ app.py
@@ -3,29 +3,32 @@ import pickle
 import pandas as pd
 import numpy as np
 
-app = Flask(__name__)
-
+app = Flask(__name__) 
 @app.route('/')
-def hello_flask(): 
-    return 'Hello flask'
+def hello_flask():
+    return 'Hello Flask'
 
-@app.route('/inicio') #renderiza página web
+@app.route('/inicio')
 def show_home():
     return render_template('index.html')
 
-@app.route('/<string: country>/<string: variety>/<float: aroma>/<float: aftertaste>/<float: acidity>/<float: body>/<float: balance>/<float: moisture>')
+@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>')
 def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
-    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
-    data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
+    cols = ['country_of_origin', 'variety', 'aroma','aftertaste','acidity','body','balance','moisture']
+    data = [country, variety, aroma,aftertaste,acidity,body,balance,moisture]
     posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
-    loaded_model = pickle.load(open('coffee_model.pkl', 'rb')) #rb es read binary
+    loaded_model = pickle.load(open('coffee_model.pkl','rb'))
     result = loaded_model.predict(posted)
-    text_result = result.tolist()[0]
-    if text_result == 'Yes':
+    test_result = result.tolist()[0]
+    if test_result == 'Yes':
         return jsonify(message = 'Your coffee is great!'), 200
     else:
         return jsonify(message = 'Your coffee sucks!'), 200
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
 
+
+# No Other/Other/7.42/7.33/7.42/7.25/7.33/0.0
+# Yes Guatemala/Bourbon/7.83/7.67/7.33/7.67/7.67/0.11
